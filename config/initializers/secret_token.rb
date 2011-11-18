929b9ef87e4e44baf27d6f60752eab61ea0e2294
Mr.Satit Rianpit <siteslave@msn.com>
2011-11-18 11:16:37 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudInventory::Application.config.secret_token = '7dda4d55b2db9ac8caa9212567926d63ec4aadee5c09c66b8fdfb7e720561b3d2eca9b6ffb0e7583cb0f72406c953fef760ed7329554cbb202bff94e16b70ee1'
